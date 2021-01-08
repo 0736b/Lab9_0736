4e4e81a6cda20fb8192f728216a63b36a1cf126d
0736b <thanawat_bumpengpun@elearning.cmu.ac.th>
2021-01-08 15:53:57 +0700

    lab9_3.cpp completed

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,34 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int main(){
+
+    ifstream filescore;
+    filescore.open("score.txt");
+    string score;
+    long double cvscore, meanscore=0,sumscore=0, norootstd_deviation = 0,sumexpo=0,rooted = 0;
+    int howmany = 0;
+
+    while(getline(filescore,score)){
+        cvscore = stold(score);
+        sumexpo+=pow(cvscore,2);
+        sumscore+=cvscore;
+        howmany+=1;
+    }
+
+    meanscore = sumscore/howmany;
+    
+
+    norootstd_deviation = (sumexpo/howmany) - (meanscore*meanscore);
+    rooted = pow(norootstd_deviation,0.5);
+
+
+    cout << "Number of data = " << howmany << "\n";
+    cout << setprecision(3);
+    cout << "Mean = " << meanscore << "\n";
+    cout << "Standard deviation = " << rooted;
+}
